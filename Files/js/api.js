7f92771ed194e11a0548757db201b808abac7b90
Filip Sufitchi <fsufitchi@gmail.com>
2015-12-19 17:53:46 -0500

    key management window complete  (yeah right)

--- Files/js/api.js
+++ Files/js/api.js
@@ -9,6 +9,8 @@ function renderKeyList() {
 	$("#nokeys-message").hide();
     }
 
+    var currentKey = window.localStorage.getItem("CURRENT_KEY_ID") || null;
+
     for (var keyId in keys) {
 	var newElement = $("#key-row-template").clone();
 	newElement.css("display", "flex");
@@ -17,7 +19,35 @@ function renderKeyList() {
 	newElement.find(".fullkey").text(keys[keyId]["key"]);
 	newElement.find(".acctname").text(keys[keyId]["name"]);
 
+	if (keyId == currentKey) {
+	    newElement.find(".current_check img").css("visibility", "visible");
+	} else {
+	    newElement.find(".current_check img").css("visibility", "hidden");
+	}
+	
 	$("#keys").append(newElement);
+
+	newElement.find(".use-button")
+	    .attr("data-keyid", keyId)
+	    .click(function() {
+		var keyId = $(this).attr("data-keyid");
+		window.localStorage.setItem("CURRENT_KEY_ID", keyId);
+		renderKeyList();
+	});
+
+	newElement.find(".delete-button")
+	    .attr("data-keyid", keyId)
+	    .click(function() {
+		var keyId = $(this).attr("data-keyid");
+		if (window.localStorage.getItem("CURRENT_KEY_ID") == keyId) {
+		    window.localStorage.removeItem("CURRENT_KEY_ID");
+		}
+		var keys = window.localStorage.getItem("API_KEYS") || "{}";
+		keys = JSON.parse(keys);
+		delete keys[keyId];
+		window.localStorage.setItem("API_KEYS", JSON.stringify(keys));
+		renderKeyList();
+	});
     }
 }
 
@@ -29,8 +59,6 @@ function addKey() {
     }
 
     gwGetAccountInfo(newKey, function(data) {
-	console.log("acc info");
-	console.log(data);
 	$("#newkey").val("");
 	var keys = window.localStorage.getItem("API_KEYS") || "{}";
 	keys = JSON.parse(keys);
@@ -38,15 +66,18 @@ function addKey() {
 	keys[data["id"]] = data;
 	keys[data["id"]]["key"] = newKey;
 
-	console.log("SETTING");
-	console.log(keys);
-	console.log(JSON.stringify(keys));
 	window.localStorage.setItem("API_KEYS", JSON.stringify(keys));
+	if (Object.keys(keys).length == 1) {
+	    // Single key, auto-select it
+	    window.localStorage.setItem("CURRENT_KEY_ID", data["id"]);
+	}
+	
 	renderKeyList();
     }, function(errdata){
 	alert("Could not add key! Try again later. " + errdata);
     });
 }
+
 
 $(document).ready(function() {
     renderKeyList();
